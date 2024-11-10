93eaea8b7c60a61d2decf69faef36e2d4aa6ea34
Steven Persia <persia.steven8@gmail.com>
2024-11-10 22:25:00 +0100

    feat: add app version

--- src/renderer/src/elements/Logo.tsx
+++ src/renderer/src/elements/Logo.tsx
@@ -1,5 +1,12 @@
-export const Logo = () => (
-  <div className="font-contrail text-sm text-white select-none">
-    <span className="font-bold">\ </span>backslash
-  </div>
-)
+import packageInfo from '../../../../package.json'
+
+export const Logo = () => {
+  const version = packageInfo.version
+
+  return (
+    <div className="font-contrail text-sm text-white select-none">
+      <span className="font-bold">\ </span>backslash
+      {version && <span className="ml-2 text-xs text-gray-400">v{version}</span>}
+    </div>
+  )
+}
